6c463fa397ffd4151d6c139a3c61bb63eac9cedb
Jesús Castro <castrojr913@gmail.com>
2018-05-14 22:55:57 -0500

    Refractor and apply minor changes

--- EvyzdaApp/app/src/main/java/esvyda/markwiliams/controller/SplashActivity.java
+++ EvyzdaApp/app/src/main/java/esvyda/markwiliams/controller/SplashActivity.java
@@ -1,8 +1,6 @@
 package esvyda.markwiliams.controller;
 
-import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,12 +17,6 @@ import esvyda.markwiliams.controller.navigation.DrawerMenuActivity_;
 public class SplashActivity extends AppCompatActivity {
 
     private static final int SPLASH_TIME = 2000; //ms
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     protected void onStart() {
